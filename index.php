e497ecbaced9b9503af2d309609695feffad3bc4
Jardel Rocha <106354939+del-iron@users.noreply.github.com>
2025-05-19 12:10:55 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <title>Kanban - Login</title>
+    <meta charset="UTF-8"> <!-- Define a codificação de caracteres como UTF-8 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Compatibilidade com IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Responsividade em dispositivos móveis -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- Otimiza carregamento de fontes Google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- Otimiza carregamento de fontes Google com CORS -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet"> <!-- Fonte Open Sans -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" /> <!-- Ícones Font Awesome -->
+    <link rel="stylesheet" href="assets/css/styles.css"> <!-- CSS principal do projeto -->
+    <title>Login - ESP</title> <!-- Título da aba do navegador -->
 </head>
 <body>
-    <section class="main-login">
-        <div class="container-login">
-            <form action="login.controller.php?action=login" method="post">
-                <p class="kanban-title">Kanban</p>
-                <input type="email" name="email" placeholder="Digite o seu email">
-                <input type="password" name="pass" placeholder="Digite a sua senha">
-                <input type="submit" value="Entrar">
-                <div class="container-p">
-                    <a href="register.php"><p>Cadastrar</p></a>
+    <section class="main-login"> <!-- Seção principal da tela de login -->
+        <div class="login-content"> <!-- Container centralizado do formulário -->
+            <form action="login.controller.php?action=login" method="post"> <!-- Formulário de login, envia para login.controller.php -->
+                <p class="kanban-title">ESP - Login</p> <!-- Título estilizado do sistema -->
+                <input type="email" name="email" placeholder="Digite seu e-mail" required> <!-- Campo para email -->
+                <input type="password" name="pass" placeholder="Digite sua senha" required> <!-- Campo para senha -->
+                <input type="submit" value="Entrar"> <!-- Botão de envio do formulário -->
+                <div class="container-p"> <!-- Container para o link de cadastro -->
+                    <a href="register.php"><p>Cadastrar</p></a> <!-- Link para tela de cadastro -->
                 </div>
             </form>
         </div>
